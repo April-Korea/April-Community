96bc6632407f097e7652bbb717d8495ccbee3986
Genn <yoon110@webchemist.net>
2017-12-14 11:23:45 +0900

    header update

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ export default {
 
 <style>
 body { margin: 0; padding: 0; background-color: #FFF; }
+a { text-decoration: none; color: inherit; }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="top">
-    <div class="title">
+    <div class="content">
+      <div class="title">
       <img src="../../static/april-korea.png" alt="">
       <span>{{ title }}</span>
     </div>
     <div class="menu">
       <ul>
-        <li>Main</li>
-        <li>Menu1</li>
-        <li>Menu2</li>
-        <li>Menu3</li>
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Project</a></li>
       </ul>
     </div>
+    <div class="gab">|</div>
     <div class="mypage">OO님 환영합니다.</div>
     <div class="logout">Logout</div>
+    </div>
   </div>
 </template>
 
@@ -29,13 +32,17 @@ export default {
 </script>
 
 <style scoped>
-  #top { position: fixed; display: flex; top: 0; left: 0; width: 100%; min-width: 1080px; height: 60px; line-height: 60px; background-color: #21252B; color: #FFF; }
-  #top .title { display: flex; padding: 0 20px; font-size: 1.4em; font-weight: 600; }
-  #top .title img { flex-basis: 44px; height: 44px; margin: 8px; }
-  #top .title span { flex: 1; }
-  #top .menu { flex: 1; }
-  #top .menu > ul { list-style-type: none; display: flex; padding: 0; margin: 0; }
-  #top .menu > ul > li { padding: 0 10px; cursor: pointer; }
-  #top .mypage { flex-basis: 120px; }
-  #top .logout { flex-basis: 100px; }
+  #top { position: fixed; top: 0; left: 0; width: 100%; min-width: 1080px; height: 300px; line-height: 300px; background-image: url('../../static/header.jpg'); background-size: cover; background-position: center; color: #FFF; }
+  #top::after { position: absolute; display: block; content: ' '; top: 0; left: 0; width: 100%; height: 100%; background-color: RGBA(0,0,0,0.4); z-index: 2; }
+  #top .content { position: relative; display: flex; width: 100%; height: 50px; line-height: 50px; z-index: 100; background: linear-gradient(RGBA(0,0,0,.9), transparent); }
+  #top .content .title { display: flex; padding: 0 20px; font-size: 1.4em; font-weight: 600; }
+  #top .content .title img { flex-basis: 30px; height: 30px; margin: 8px; }
+  #top .content .title span { flex: 1; }
+  #top .content .menu { flex: 1; }
+  #top .content .menu > ul { list-style-type: none; display: flex; justify-content: flex-end; padding: 0; margin: 0; }
+  #top .content .menu > ul > li { padding: 0 10px; cursor: pointer; font-weight: 600; }
+  #top .content .menu > ul > li.active a { border-bottom: 2px solid #FFF; }
+  #top .content .gab { flex-basis: 30px; text-align: center; }
+  #top .content .mypage { flex-basis: 120px; font-weight: 600; }
+  #top .content .logout { flex-basis: 100px; font-weight: 600; }
 </style>
